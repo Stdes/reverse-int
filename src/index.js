82a33099017f837940ca35d1c5eca13eb35f0f18
Stanislav <stanislav.msb@yandex.ru>
2023-09-02 22:51:52 +1000

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
   
+
+    let str = toString(n);
+    let reverseStr = '';
+
+    for (let i = str.length; i=1; i = i - 1) {
+        reverseStr = reverseStr + str[i];
+    }
+    return reverseStr;
 }
